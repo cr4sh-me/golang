ebc89931546508805aae8ab5d1485dc0fcff37b2
cr4sh-me <cr4sh.lab@gmail.com>
2024-05-28 15:35:50 +0200

    update

--- setup.sh
+++ setup.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-GO_PATH="/usr/lib/go"
+GO_PATH="/opt/cybertools/golang/go"
 GO_VERSION="go1.22.3"
 TARGET_ARCH="$(uname -m)"
-
-if [ "$(id -u)" -ne 0 ]; then
-    echo "Run this as root idiot!"
-    exit 1
-fi
 
 if [ "$TARGET_ARCH" == "aarch64" ]; then
     FILE_ARCH="arm64"
@@ -21,10 +16,8 @@ fi
 FILENAME="$GO_VERSION.linux-$FILE_ARCH.tar.gz"
 
 rm -rf $GO_PATH
-mkdir -p $GO_PATH
-
 wget "https://go.dev/dl/$FILENAME"
-tar -xvf "$FILENAME" -C /usr/lib
+tar -xvf "$FILENAME"
 rm -f "$FILENAME"
 
-$GO_PATH/bin/go env -w GOPATH="$HOME/.go"
+$GO_PATH/bin/go env -w GOPATH="$GO_PATH/.go"
